76242c46ebeab87414c8893e2b96cfc5b7ca45ba
Tim <tim.schrader@frickly.systems>
2025-09-02 13:19:58 +0200

    [wip]

--- tests/lib/uds_new/src/test_0x22_read_data_by_id.c
+++ tests/lib/uds_new/src/test_0x22_read_data_by_id.c
@@ -131,7 +131,51 @@ ZTEST_F(lib_uds_new, test_0x22_read_by_id_both_actions_are_executed) {
 }
 
 //////////////////////7
+UDSErr_t custom_check_for_0x22_returns_action_returncode(
+    const struct uds_new_context *const context, bool *apply_action) {
+  if (context->registration->type ==
+          UDS_NEW_REGISTRATION_TYPE__DATA_IDENTIFIER &&
+      context->registration->data_identifier.data_id == data_id_r &&
+      context->event == UDS_EVT_ReadDataByIdent) {
+    *apply_action = true;
+  }
+  return UDS_OK;
+}
 
+UDSErr_t custom_action_for_0x22_returns_action_returncode(
+    struct uds_new_context *const context, bool *consume_event) {
+  if (context->registration->type ==
+          UDS_NEW_REGISTRATION_TYPE__DATA_IDENTIFIER &&
+      context->registration->data_identifier.data_id == data_id_r &&
+      context->event == UDS_EVT_ReadDataByIdent) {
+    *consume_event = false;
+    return UDS_NRC_SecurityAccessDenied;
+  }
+  return UDS_OK;
+}
+
+ZTEST_F(lib_uds_new, test_0x22_read_by_id_returns_action_returncode) {
+  struct uds_new_instance_t *instance = fixture->instance;
+
+  data_id_check_fn_fake.custom_fake =
+      custom_check_for_0x22_returns_action_returncode;
+  data_id_action_fn_fake.custom_fake =
+      custom_action_for_0x22_returns_action_returncode;
+
+  uint32_t data = 0x11223344;
+
+  UDSWDBIArgs_t arg = {
+    .dataId = data_id_rw,
+    .data = (uint8_t *)&data,
+    .len = sizeof(data),
+  };
+
+  int ret = receive_event(instance, UDS_EVT_ReadDataByIdent, &arg);
+  zassert_equal(ret, UDS_NRC_SecurityAccessDenied);
+
+  zassert_true(data_id_check_fn_fake.call_count >= 1);
+  zassert_equal(data_id_action_fn_fake.call_count, 1);
+}
 //////////////////////7
 #ifdef CONFIG_UDS_NEW_USE_DYNAMIC_REGISTRATION
 

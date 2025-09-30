b6a3c2b59fd491db5ddfdd0d475a8dc73483fa20
Fabian Gajek <fabian.gajek@frickly.systems>
2025-09-30 10:01:16 +0000

    wip
    Signed-off-by: Fabian Gajek <fabian.gajek@frickly.systems>

---
+++ lib/uds/upload_download_file_transfer.h
@@ -0,0 +1,12 @@
+#ifndef UDS_UPLOAD_DOWNLOAD_FILE_TRANSFER_H
+#define UDS_UPLOAD_DOWNLOAD_FILE_TRANSFER_H
+
+#include <ardep/uds.h>
+#include "uds.h"
+
+bool uds_file_transfer_is_active(void);
+UDSErr_t uds_file_transfer_request(struct uds_context* const context);
+UDSErr_t uds_file_transfer_continue(struct uds_context* const context);
+UDSErr_t uds_file_transfer_exit(void);
+
+#endif // UDS_UPLOAD_DOWNLOAD_FILE_TRANSFER_H

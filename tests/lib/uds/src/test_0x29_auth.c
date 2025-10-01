87ae40ffcfa12c84fae32cc9dac59f21537a4458
Tim Schrader <tim.schrader@frickly.systems>
2025-10-01 13:46:02 +0200

    tests
    Signed-off-by: Tim Schrader <tim.schrader@frickly.systems>

--- tests/lib/uds/src/test_0x29_auth.c
+++ tests/lib/uds/src/test_0x29_auth.c
@@ -38,33 +38,32 @@ UDSErr_t auth_0x29_action_fn(struct uds_context *const context,
     switch (args->type) {
       case UDS_LEV_AT_AC:
         return args->set_auth_state(context->server, UDS_AT_ACAPCE);
-      case UDS_LEV_AT_VCU:
-
-        // return args->set_auth_state(context->server, UDS_AT_VCU);
       default:
         return UDS_NRC_SubFunctionNotSupported;
     }
 
     return UDS_PositiveResponse;
   } else if (context->event == UDS_EVT_AuthTimeout) {
+    *consume_event = true;
     return UDS_PositiveResponse;
   }
   return UDS_NRC_SubFunctionNotSupported;
 }
 
-// ZTEST_F(lib_uds, test_0x29_auth_timeout) {
-//   struct uds_instance_t *instance = fixture->instance;
+ZTEST_F(lib_uds, test_0x29_auth_timeout) {
+  struct uds_instance_t *instance = fixture->instance;
 
-//   data_id_check_fn_fake.custom_fake = auth_0x29_check_fn;
-//   data_id_action_fn_fake.custom_fake = auth_0x29_action_fn;
+  data_id_check_fn_fake.custom_fake = auth_0x29_check_fn;
+  data_id_action_fn_fake.custom_fake = auth_0x29_action_fn;
 
-//   int ret = receive_event(instance, UDS_EVT_AuthTimeout, NULL);
-//   zassert_ok(ret);
+  uint32_t action_call_count = data_id_action_fn_fake.call_count;
 
-//   assert_auth_state(UDS_AT_ACAPCE);
-// }
+  int ret = receive_event(instance, UDS_EVT_AuthTimeout, NULL);
+  zassert_ok(ret);
+  zassert_true(data_id_action_fn_fake.call_count > action_call_count);
+}
 
-ZTEST_F(lib_uds, test_0x29_auth) {
+ZTEST_F(lib_uds, test_0x29_auth_get_auch_config) {
   struct uds_instance_t *instance = fixture->instance;
 
   data_id_check_fn_fake.custom_fake = auth_0x29_check_fn;
@@ -81,15 +80,6 @@ ZTEST_F(lib_uds, test_0x29_auth) {
   int ret = receive_event(instance, UDS_EVT_Auth, &args);
   zassert_ok(ret);
   zassert_equal(set_auth_state_fake.call_count, 1);
-  zassert_true(action_call_count < data_id_action_fn_fake.call_count);
-
-  uint8_t certificate[] = {0x00, 0x11, 0x22, 0x33};
-  uint8_t challenge[] = {0x44, 0x55, 0x66, 0x77, 0x88};
-  args.type = UDS_LEV_AT_VCU;
-  args.subFuncArgs.verifyCertArgs.cert = certificate;
-  args.subFuncArgs.verifyCertArgs.certLen = sizeof(certificate);
-  args.subFuncArgs.verifyCertArgs.challenge = challenge;
-  args.subFuncArgs.verifyCertArgs.challengeLen = sizeof(challenge);
-
+  zassert_true(data_id_action_fn_fake.call_count > action_call_count);
   assert_auth_state(UDS_AT_ACAPCE);
 }

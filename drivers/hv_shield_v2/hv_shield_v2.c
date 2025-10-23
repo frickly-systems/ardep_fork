4b710431149164f9b9c7c581740e37befde577bc
Mark Oude Elberink <mark.oude.elberink@frickly.systems>
2025-10-23 14:54:48 +0200

    wip: initial interrupts
    Signed-off-by: Mark Oude Elberink <mark.oude.elberink@frickly.systems>

--- drivers/hv_shield_v2/hv_shield_v2.c
+++ drivers/hv_shield_v2/hv_shield_v2.c
@@ -13,6 +13,14 @@
 
 LOG_MODULE_REGISTER(hv_shield, CONFIG_HV_SHIELD_V2_LOG_LEVEL);
 
+#define REG_IODIRA 0x00
+#define REG_IOCONA 0x0A
+
+#define REG_INTCONA 0x08
+#define REG_DEFVALA 0x06
+#define REG_GPINTENA 0x04
+#define REG_GPIOA 0x12
+
 struct hv_shield_v2_config {
   struct gpio_driver_config common;
   struct i2c_dt_spec i2c;
@@ -22,25 +30,30 @@ struct hv_shield_v2_config {
 
 struct hv_shield_v2_data {
   struct gpio_driver_data common;
+  const struct device*
+      device;  // back reference to device struct (used in work handler)
   sys_slist_t interrupt_callbacks;
+  struct gpio_callback interrupt_gpio_cb;
 
   struct {
-    uint8_t gpintena;
-    uint8_t gpintenb;
-    // defval
-    uint8_t intcona;  // =0
-    uint8_t intconb;
+    uint16_t gpinten;
+    uint16_t intcon;
+    uint16_t defval;
     uint16_t gpio;
-
   } reg_cache;
+
+  uint16_t int_trigger_rising;
+  uint16_t int_trigger_falling;
+
+  struct k_work on_interrupt_work;
 };
 
 static int write_iocon(const struct hv_shield_v2_config* config,
                        uint8_t value) {
-  uint8_t buf[3] = {0x0A, value, value};
+  uint8_t buf[3] = {REG_IOCONA, value, value};
   int ret = i2c_write_dt(&config->i2c, buf, sizeof(buf));
   if (ret) {
-    LOG_ERR("Failed to write IOCON register 0x0A: %d", ret);
+    LOG_ERR("Failed to write IOCON register " STRINGIFY(REG_IOCONA)": %d", ret);
   }
   return ret;
 }
@@ -70,13 +83,64 @@ static int read_u16_reg(const struct hv_shield_v2_config* config,
   return 0;
 }
 
+static void hv_shield_v2_int_gpio_handler(const struct device* port,
+                                          struct gpio_callback* cb,
+                                          gpio_port_pins_t pins) {
+  struct hv_shield_v2_data* data =
+      CONTAINER_OF(cb, struct hv_shield_v2_data, interrupt_gpio_cb);
+
+  k_work_submit(&data->on_interrupt_work);
+}
+
+static void hv_shield_v2_interrupt_work_handler(struct k_work* work) {
+  struct hv_shield_v2_data* data =
+      CONTAINER_OF(work, struct hv_shield_v2_data, on_interrupt_work);
+  const struct device* dev = data->device;
+  const struct hv_shield_v2_config* config = dev->config;
+
+  // todo: read registers
+  uint16_t intf = 0;
+  int err = read_u16_reg(config, 0x0E, &intf);
+  if (err) {
+    LOG_ERR("Error handling interrupt; could not read register: %d", err);
+    return;
+  }
+
+  if (!intf) {
+    LOG_DBG("Interrupt was not for this IC");
+    return;
+  }
+
+  uint16_t intcap = 0;
+  err = read_u16_reg(config, 0x10, &intcap);
+
+  // todo: optimize interrupt detection
+  const uint16_t level_interrupts =
+      data->reg_cache.gpinten & data->reg_cache.intcon;
+  const uint16_t rising_edge_interrupts =
+      data->reg_cache.gpinten & intcap & data->int_trigger_rising;
+  const uint16_t falling_edge_interrupts =
+      data->reg_cache.gpinten & (~intcap) & data->int_trigger_falling;
+
+  const uint16_t ints = intf & (level_interrupts | rising_edge_interrupts |
+                                falling_edge_interrupts);
+
+  gpio_fire_callbacks(&data->interrupt_callbacks, dev, ints);
+}
+
 static int hv_shield_v2_init(const struct device* dev) {
   const struct hv_shield_v2_config* config = dev->config;
+  struct hv_shield_v2_data* data = dev->data;
+
+  data->device = dev;
 
   if (!device_is_ready(config->i2c.bus)) {
     LOG_ERR("I2C bus %s not ready", config->i2c.bus->name);
     return -ENODEV;
   }
+
+  gpio_init_callback(&data->interrupt_gpio_cb, hv_shield_v2_int_gpio_handler,
+                     0);
 
   for (int i = 0; i < config->int_gpio_count; i++) {
     if (!device_is_ready(config->int_gpios[i].port)) {
@@ -104,12 +168,14 @@ static int hv_shield_v2_init(const struct device* dev) {
   }
 
   // Write IODIR registers (1 is input, 0 is output)
-  if (write_u16_reg(config, 0x00, 0b0111111101000000) != 0) {
+  if (write_u16_reg(config, REG_IODIRA, 0b0111111101000000) != 0) {
     LOG_ERR("Failed to write IODIR registers");
     return -EIO;
   }
 
   LOG_INF("HV Shield v2 initialized on I2C address 0x%02x", config->i2c.addr);
+
+  k_work_init(&data->on_interrupt_work, hv_shield_v2_interrupt_work_handler);
 
   return 0;
 }
@@ -159,7 +225,7 @@ static int hv_shield_v2_port_get_raw(const struct device* port,
   struct hv_shield_v2_data* data = port->data;
 
   uint16_t reg_value;
-  if (read_u16_reg(config, 0x12, &reg_value) != 0) {
+  if (read_u16_reg(config, REG_GPIOA, &reg_value) != 0) {
     return -EIO;
   }
 
@@ -198,7 +264,7 @@ static int hv_shield_v2_gpio_set_masked_raw(const struct device* port,
   const uint16_t new_gpio =
       (data->reg_cache.gpio & ~mapped_mask) | (mapped_value & mapped_mask);
 
-  if (write_u16_reg(config, 0x12, new_gpio) != 0) {
+  if (write_u16_reg(config, REG_GPIOA, new_gpio) != 0) {
     return -EIO;
   }
 
@@ -264,9 +330,10 @@ static int hv_shield_v2_pin_configure(const struct device* dev,
         data->reg_cache.gpio &= ~(1 << bit);
       }
 
-      if (write_u16_reg(config, 0x12, data->reg_cache.gpio) != 0) {
+      if (write_u16_reg(config, REG_GPIOA, data->reg_cache.gpio) != 0) {
         return -EIO;
       }
+
       break;
 
     // todo: do fault pins have to be something else?
@@ -294,6 +361,85 @@ static int hv_shield_v2_manage_callback(const struct device* dev,
   return gpio_manage_callback(&data->interrupt_callbacks, cb, set);
 }
 
+static int hv_shield_v2_pin_interrupt_configure(const struct device* port,
+                                                gpio_pin_t pin,
+                                                enum gpio_int_mode mode,
+                                                enum gpio_int_trig trig) {
+  struct hv_shield_v2_data* data = port->data;
+  if ((pin & HV_SHIELD_V2_BASE_MASK) != HV_SHIELD_V2_INPUT_BASE &&
+      (pin & HV_SHIELD_V2_BASE_MASK) != HV_SHIELD_V2_FAULT_BASE) {
+    LOG_ERR("Interrupts can only be configured on input and fault pins");
+    return -ENOTSUP;
+  }
+  uint8_t bit = hv_shield_v2_pin_to_bit(pin);
+
+  uint16_t defval = data->reg_cache.defval;
+  uint16_t intcon =
+      data->reg_cache.intcon;  // 0 = on change, 1 = compare to defval
+  uint16_t gpinten = data->reg_cache.gpinten;  // 0 = disabled, 1 = enabled
+
+  switch (mode) {
+    case GPIO_INT_MODE_DISABLED:
+      gpinten &= ~BIT(bit);
+      break;
+    case GPIO_INT_MODE_LEVEL:
+      gpinten |= BIT(bit);
+      intcon |= BIT(bit);
+      switch (trig) {
+        case GPIO_INT_TRIG_LOW:
+          defval &= ~BIT(bit);
+          break;
+        case GPIO_INT_TRIG_HIGH:
+          defval |= BIT(bit);
+          break;
+        default:
+          LOG_ERR("Invalid trigger for level interrupt");
+          return -EINVAL;
+      }
+      break;
+    case GPIO_INT_MODE_EDGE:
+      gpinten |= BIT(bit);
+      intcon &= ~BIT(bit);
+      switch (trig) {
+        case GPIO_INT_TRIG_HIGH:  // rising edge
+          data->int_trigger_rising |= BIT(bit);
+          data->int_trigger_falling &= ~BIT(bit);
+          break;
+        case GPIO_INT_TRIG_LOW:  // falling edge
+          data->int_trigger_falling |= BIT(bit);
+          data->int_trigger_rising &= ~BIT(bit);
+          break;
+        case GPIO_INT_TRIG_BOTH:
+          data->int_trigger_rising |= BIT(bit);
+          data->int_trigger_falling |= BIT(bit);
+          break;
+        default:
+          LOG_ERR("Invalid trigger for edge interrupt");
+          return -EINVAL;
+      }
+      break;
+    default:
+      LOG_ERR("Invalid interrupt mode");
+      return -EINVAL;
+  }
+
+  if (write_u16_reg(port->config, REG_DEFVALA, defval) != 0) {
+    return -EIO;
+  }
+  if (write_u16_reg(port->config, REG_INTCONA, intcon) != 0) {
+    return -EIO;
+  }
+  if (write_u16_reg(port->config, REG_GPINTENA, gpinten) != 0) {
+    return -EIO;
+  }
+
+  data->reg_cache.defval = defval;
+  data->reg_cache.intcon = intcon;
+  data->reg_cache.gpinten = gpinten;
+
+  return 0;
+}
+
 DEVICE_API(gpio, hv_shield_api) = {
   .pin_configure = hv_shield_v2_pin_configure,
   .port_get_raw = hv_shield_v2_port_get_raw,
@@ -301,6 +447,7 @@ DEVICE_API(gpio, hv_shield_api) = {
   .port_set_bits_raw = hv_shield_v2_gpio_set_bits_raw,
   .port_clear_bits_raw = hv_shield_v2_gpio_clear_bits_raw,
   .manage_callback = hv_shield_v2_manage_callback,
+  .pin_interrupt_configure = hv_shield_v2_pin_interrupt_configure,
 };
 
 // todo: .common init; todo: interrupt pin stuff
@@ -319,10 +466,9 @@ DEVICE_API(gpio, hv_shield_api) = {
   static struct hv_shield_v2_data hv_shield_##x##_data = {                 \
     .reg_cache =                                                           \
         {                                                                  \
-          .gpintena = 0,                                                   \
-          .gpintenb = 0,                                                   \
-          .intcona = 0,                                                    \
-          .intconb = 0,                                                    \
+          .gpinten = 0,                                                    \
+          .intcon = 0,                                                     \
+          .defval = 0,                                                     \
           .gpio = 0,                                                       \
         },                                                                 \
   };                                                                       \

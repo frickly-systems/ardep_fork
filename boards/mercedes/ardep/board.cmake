c308f427cdb17f5d7046a5d5b7e5c534615e6bac
Tim Schrader <tim.schrader@frickly.systems>
2025-10-23 09:44:31 +0200

    distinguish flash runners (blackmagic and ardep)
    Signed-off-by: Tim Schrader <tim.schrader@frickly.systems>

--- boards/mercedes/ardep/board.cmake
+++ boards/mercedes/ardep/board.cmake
@@ -3,13 +3,25 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+
+message(STATUS "BOARD_REVISION: ${BOARD_REVISION}")
+message(STATUS "ACTIVE_BOARD_REVISION: ${ACTIVE_BOARD_REVISION}")
+
 board_runner_args(jlink "--device=STM32G474VE" "--speed=4000" "--reset-after-load")
 
 # TODO: validate settings
 board_runner_args(pyocd "--target=stm32g474retx")
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 
-include(${CMAKE_CURRENT_LIST_DIR}/../../common/ardep.board.cmake)
+
+if(BOARD_REVISION STREQUAL "1.0.0")
+    include(${CMAKE_CURRENT_LIST_DIR}/../../common/ardep.board.cmake)
+elseif(BOARD_REVISION STREQUAL "2.0.0")
+    include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
+endif()
+# include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
+# include(${CMAKE_CURRENT_LIST_DIR}/../../common/ardep.board.cmake)
+
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)

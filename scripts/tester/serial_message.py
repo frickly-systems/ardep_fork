ebc92469c0c2b6990d27fb491d5ae03ee592b9cc
Tim Schrader <tim.schrader@frickly.systems>
2025-10-27 11:49:58 +0100

    allow and ignore Boot Banner messages
    Signed-off-by: Tim Schrader <tim.schrader@frickly.systems>

--- scripts/tester/serial_message.py
+++ scripts/tester/serial_message.py
@@ -5,6 +5,13 @@ import logging
 log = logging.getLogger(__name__ + ".serial_message")
 
 
+class BootBannerFound(Exception):
+    banner: str
+
+    def __init__(self, banner: str):
+        self.banner = banner
+
+
 class SerialMessage:
     time_ms: int = None
     log_level: str = None
@@ -13,6 +20,10 @@ class SerialMessage:
 
     def __init__(self, message: str):
         try:
+            if message.startswith("***") and message.endswith("***"):
+                log.info("Found Boot banner: %s", message)
+                raise BootBannerFound(message)
+
             raw_timestamp, raw_level, raw_device, raw_message = message.split(" ", 3)
             self.time_ms = int(raw_timestamp.strip("[]"))
             self.log_level = raw_level.strip("<>")

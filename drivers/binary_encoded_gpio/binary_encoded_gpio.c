9c5761af22e0e43a23121eec4ad7b502efa6a6ed
Mark Oude Elberink <mark.oude.elberink@frickly.systems>
2025-12-22 16:28:22 +0100

    feat: update gearshift driver to support a variable amount of gpios
    Signed-off-by: Mark Oude Elberink <mark.oude.elberink@frickly.systems>

--- drivers/binary_encoded_gpio/binary_encoded_gpio.c
+++ drivers/binary_encoded_gpio/binary_encoded_gpio.c
@@ -13,19 +13,21 @@
 
 #include <ardep/drivers/binary_encoded_gpio.h>
 
+// number of bits for an integer minus sign bit
+#define MAXIMUM_SUPPORTED_PIN_COUNT (sizeof(int) * 8 - 1)
+
 LOG_MODULE_REGISTER(binary_encoded_gpio, CONFIG_BINARY_ENCODED_GPIO_LOG_LEVEL);
 
-#define BINARY_ENCODED_GPIO_PIN_COUNT 3
-
 struct binary_encoded_gpio_config {
-  struct gpio_dt_spec input_pins[BINARY_ENCODED_GPIO_PIN_COUNT];
+  const struct gpio_dt_spec *input_pins;
+  size_t pin_count;
 };
 
 static int get_value(const struct device *device) {
   const struct binary_encoded_gpio_config *config = device->config;
   int value = 0;
 
-  for (int i = 0; i < BINARY_ENCODED_GPIO_PIN_COUNT; i++) {
+  for (int i = 0; i < config->pin_count; i++) {
     int ret = gpio_pin_get_dt(&config->input_pins[i]);
     if (ret < 0) {
       LOG_ERR("Failed to read input pin %d: %d", i, ret);
@@ -45,7 +47,7 @@ static int binary_encoded_gpio_init(const struct device *dev) {
 
   LOG_DBG("Initializing binary encoded GPIO driver");
 
-  for (int i = 0; i < BINARY_ENCODED_GPIO_PIN_COUNT; i++) {
+  for (int i = 0; i < config->pin_count; i++) {
     if (!device_is_ready(config->input_pins[i].port)) {
       return -ENODEV;
     }
@@ -69,21 +71,23 @@ BUILD_ASSERT(
     CONFIG_BINARY_ENCODED_GPIO_INIT_PRIORITY > CONFIG_GPIO_INIT_PRIORITY,
     "BINARY_ENCODED_GPIO_INIT_PRIORITY must be higher than GPIO_INIT_PRIORITY");
 
-#define BINARY_ENCODED_GPIO_INIT(inst)                                      \
-  static const struct binary_encoded_gpio_config                            \
-      binary_encoded_gpio_config_##inst = {                                 \
-        .input_pins =                                                       \
-            {                                                               \
-              DT_INST_FOREACH_PROP_ELEM_SEP(inst, input_gpios,              \
-                                            GPIO_DT_SPEC_GET_BY_IDX, (, )), \
-            },                                                              \
-  };                                                                        \
-  BUILD_ASSERT(                                                             \
-      DT_INST_PROP_LEN(inst, input_gpios) == BINARY_ENCODED_GPIO_PIN_COUNT, \
-      "Binary encoded GPIO driver requires exactly " STRINGIFY(BINARY_ENCODED_GPIO_PIN_COUNT) " input_gpios");                   \
-  DEVICE_DT_INST_DEFINE(inst, binary_encoded_gpio_init, NULL, NULL,         \
-                        &binary_encoded_gpio_config_##inst, POST_KERNEL,    \
-                        CONFIG_BINARY_ENCODED_GPIO_INIT_PRIORITY,           \
+#define BINARY_ENCODED_GPIO_INIT(inst)                                        \
+  static const struct gpio_dt_spec input_pins_##inst[] = {                    \
+    DT_INST_FOREACH_PROP_ELEM_SEP(inst, input_gpios, GPIO_DT_SPEC_GET_BY_IDX, \
+                                  (, )),                                      \
+  };                                                                          \
+  static const struct binary_encoded_gpio_config                              \
+      binary_encoded_gpio_config_##inst = {                                   \
+        .input_pins = input_pins_##inst,                                      \
+        .pin_count = DT_INST_PROP_LEN(inst, input_gpios),                     \
+  };                                                                          \
+  BUILD_ASSERT(                                                               \
+      DT_INST_PROP_LEN(inst, input_gpios) <= MAXIMUM_SUPPORTED_PIN_COUNT,     \
+      "Binary encoded GPIO driver can use at "                                \
+      "most " STRINGIFY(MAXIMUM_SUPPORTED_PIN_COUNT) " input_gpios");         \
+  DEVICE_DT_INST_DEFINE(inst, binary_encoded_gpio_init, NULL, NULL,           \
+                        &binary_encoded_gpio_config_##inst, POST_KERNEL,      \
+                        CONFIG_BINARY_ENCODED_GPIO_INIT_PRIORITY,             \
                         &binary_encoded_gpio_api);
 
 DT_INST_FOREACH_STATUS_OKAY(BINARY_ENCODED_GPIO_INIT);
